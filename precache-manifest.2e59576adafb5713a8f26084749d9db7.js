4ab87f5a236e90f0562af03f2d83820ee3d3b4bb
hungpn256 <phamnanghung.25@gmail.com>
2020-06-28 18:08:20 +0700

    first commit

---
+++ precache-manifest.2e59576adafb5713a8f26084749d9db7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d299167e79a92487f64c3753e6161fa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c26adbcbed7cbf1bfc1",
+    "url": "/static/css/main.b18637f0.chunk.css"
+  },
+  {
+    "revision": "ee44aa7e45e19eb53671",
+    "url": "/static/js/2.03974b98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03974b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c26adbcbed7cbf1bfc1",
+    "url": "/static/js/main.2eca1bb2.chunk.js"
+  },
+  {
+    "revision": "4e5eb82d3a67a88f3886",
+    "url": "/static/js/runtime-main.829a5a48.js"
+  }
+]);
